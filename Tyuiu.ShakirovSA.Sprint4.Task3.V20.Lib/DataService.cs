3137babae2eb05b0cd74abbcc2efbc47dc288bfa
Qasqader <Tyuiu.ShakirovSA@outlook.com>
2025-11-09 17:21:27 +0300

    + task3

---
+++ Tyuiu.ShakirovSA.Sprint4.Task3.V20.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ShakirovSA.Sprint4.Task3.V20.Lib
+{
+    public class DataService : ISprint4Task3V20
+    {
+        public int Calculate(int[,] array)
+        {
+            int min = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (array[i, 0] < min)
+                {
+                    min = array[i, 0];
+                }
+            }
+            return min;
+
+        }
+    }
+}
---
+++ Tyuiu.ShakirovSA.Sprint4.Task3.V20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.ExceptionServices;
+using Tyuiu.ShakirovSA.Sprint4.Task3.V20.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int[,] array = { { 8, 7, 7, 8, 5 }, { 4, 3, 5, 3, 6 }, { 5, 3, 8, 6, 3 }, { 6, 3, 8, 5, 4 }, { 3, 6, 8, 3, 4 } };
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for(int j = 0;  j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]} \t ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Исходная матрица");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(array);
+        Console.WriteLine("Ответ: " + res);
+        Console.ReadKey();
+    }
+}
