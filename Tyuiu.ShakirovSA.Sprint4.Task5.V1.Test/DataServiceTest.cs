597c739503409e64379210b64ab4f0f5bf71bd82
Qasqader <Tyuiu.ShakirovSA@outlook.com>
2025-11-11 20:31:37 +0300

    +task 5

---
+++ Tyuiu.ShakirovSA.Sprint4.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShakirovSA.Sprint4.Task5.V1.Lib;
+namespace Tyuiu.ShakirovSA.Sprint4.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[,] matr = { { 2, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+            int[,] res = ds.Calculate(matr);
+            int[,] wait = { { 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.ShakirovSA.Sprint4.Task5.V1/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.ShakirovSA.Sprint4.Task5.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.Write("Введите количество строк в массиве");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите количество столбцов в массиве");
+        int cols = Convert.ToInt32(Console.ReadLine());
+        int[,] mtrx = new int[rows, cols];
+        Console.WriteLine();
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                mtrx[i, j] = rnd.Next(-9, 8);
+            }
+        }
+        Console.WriteLine("\n Массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Исходная матрица");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int[,] res = ds.Calculate(mtrx);
+        Console.WriteLine("Результирующая матрица");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                Console.Write($"{res[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
